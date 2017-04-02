e8cc16b184e0c01f45026943a2cca8a9d3f2b9e9
Kim <kimeh11f@naver.com>
2017-04-02 17:57:08 +0900

    비정상종료시 체크를 위해 서버연결 Timeout설정

--- server.js
+++ server.js
@@ -21,44 +21,49 @@ app.get('/', function(req, res){
 
 
 //소켓 서버 생성
-var io = socketio.listen(server);
+var io = socketio.listen(server, {
+    'pingInterval': 2000,
+    'pingTimeout': 2000
+});
 var id = 0;
 var peopleCnt = 0;
+//클라이언트가 연결되었을 때
 io.sockets.on('connection', function (socket) {
-    console.log(socket.id + " 접속 ("+ socket.request.connection.remoteAddress+")" );
+    console.log(socket.id + " 접속 (" + socket.request.connection.remoteAddress + ")");
     peopleCnt++;
     io.sockets.emit('setPeopleCnt', { peopleCnt: peopleCnt});
-    //message 이벤트
+    //연결된 클라이언트 전체에 broadcast
     socket.on('message', function(data){
-        //클라이언트의 message 이벤트를 발생시킵니다.
         console.log("아이디 : " + data.name + ", 내용 : " + data.message + ", 날짜 : " + data.date + "(" + socket.request.connection.remoteAddress +")"); 
         data.socketId = socket.id;
         io.sockets.emit('message', data);
     });
 
+    //나머지 클라이언트 전체에 broadcast
     socket.on('pushLocation', function (data) {
-        //클라이언트의 message 이벤트를 발생시킵니다.
         console.log("latitude : " + data.latitude + ", longitude : " + data.longitude);
         id = socket.id;
         data.id = id;
         socket.broadcast.emit('addMarker', data);
     });
 
+    //대상 소켓에 위치를 응답
     socket.on('replyLocation', function (data) {
-        //클라이언트의 message 이벤트를 발생시킵니다.
         if (id != 0) {
             io.sockets.to(id).emit('initMap', data);
         }
     });
+
+    //위치공유창 닫았을 때 marker삭제 이벤트 발생시킴.
     socket.on('closeMap', function (data) {
-        //클라이언트의 message 이벤트를 발생시킵니다.
         socket.broadcast.emit('deleteMarker', data);
     });
 
+    //소켓이 접속종료했을 때 접속자수 줄이고 marker삭제 이벤트 발생시킴.
     socket.on('disconnect', function () {
         peopleCnt--;
         console.log(socket.id + " 접속 종료 (" + socket.request.connection.remoteAddress + ")");
-        //클라이언트의 message 이벤트를 발생시킵니다.
+        
         socket.broadcast.emit('deleteMarker', {
             socketId: socket.id
         });
